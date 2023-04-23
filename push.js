3077d27c1e183a992cd138ca41f82ace38ef395c
KJafro <Kurtj222@outlook.com>
2023-04-23 10:59:17 +0100

    ok

--- push.js
+++ push.js
@@ -1,12 +1,12 @@
 var push = require('web-push')
 
 let vapidKeys = {
-    publicKey: 'BB6iMHJgjroOX91pDJ4EAlsYCuiDc2OSVsEoWZiXv77GxAuWRny6ZubRMDgP0jw1k96LgrdHSbDl3kxw2rIjsag',
-    privateKey: '1HhlHfhFBaPloDx1mx5NY-J391wIDXkRmEqNCVgY_nI'
-}
+    publicKey: 'BOU-dlKoy3-aFElyPR4SLO8d6b4T7JvE9cAdiJJzKOiGysrvx6ddZ5FCwASvgCII1cWR4Aml3AKyRto33GSh16Q',
+    privateKey: 'OUHyjmcDqBgsCDtrAeUGqQoAfRnKXCC-ibx6OyFrGBw'
+  }
 
-push.setVapidDetails('mailto: test@test.com', vapidKeys.publicKey, vapidKeys.privateKey)
+  push.setVapidDetails('mailto: ok@aol.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/e3s9TMqAjRk:APA91bF0Aq8D184ZxpLPj1GWo0HOsoMDf7MIlOMTe0KBFgDlpqugWvednxN--hb7h-JWJ6hh_J2FjOnepDS5wIsVMc-hoFiGyKPINyPC44TUyPIGCYIo3h_IWEnxp_-f5igntY89EAcC","expirationTime":null,"keys":{"p256dh":"BPWSAdZQ-_E7Qow95uRlXEbHE045PVgUCXC5kVwc8DQsKH4hFKLDXRcX14hWVOTdsjMkWTuZ7zaKMIpLPy8MnYc","auth":"c13GgNx7pi9jh5OvvfJ1zw"}}
+  let sub = {}
 
-push.sendNotification(sub, 'test!!!!!!')
+  push.sendNotification(sub, 'testing message')
